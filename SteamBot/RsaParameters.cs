6e7a19f75aef256476e78ce6cfcf9ff4f218fce2
CooLe <coole_vk@mail.ru>
2020-11-13 12:28:11 +0200

    Start

---
+++ SteamBot/RsaParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamBot
+{
+   public class RsaParameters
+    {
+        public string Exponent;
+        public string Modulus;
+        public string Password;
+    }
+}
