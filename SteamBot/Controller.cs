c8259aae11fad9f31b789b05e8e61a6f46ec8381
CooLe <coole_vk@mail.ru>
2020-11-20 13:58:04 +0200

    Ф-я получения макс стр. в топике

--- SteamBot/Controller.cs
+++ SteamBot/Controller.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SteamBot
 {
--- SteamBot/SpamTopic.cs
+++ SteamBot/SpamTopic.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Http;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 
@@ -19,14 +24,14 @@ namespace SteamBot
         {
             while(true)
             {
-                var source = await GetHtmlPage();
+                GetMaxPage();
 
 
                 //request = await client.GetAsync($"https://steamcommunity.com/groups/SearchForFriends/discussions/0/364043054108978276");
 
                 //result = await request.Content.ReadAsStringAsync();
 
-               
+
 
 
 
@@ -36,6 +41,40 @@ namespace SteamBot
             }
         }
 
+
+        private int GetMaxPage()
+        {
+            IWebDriver webDriver;
+
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+
+            //off notifications
+            options.AddArguments("--disable-notifications");
+
+            //hide Chrome
+            options.AddArguments("--headless");
+
+            //Options wd
+            webDriver = new ChromeDriver(driverService, options);
+
+            webDriver.Navigate().GoToUrl("https://steamcommunity.com/groups/SearchForFriends/discussions/0/364043054108978276");
+          
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> wbElement = webDriver.FindElements(By.ClassName("commentthread_pagelink"));
+
+            webDriver.Close();
+            webDriver.Quit();
+
+            List<int> listWithPages = new List<int>();
+
+            foreach(IWebElement tmp in wbElement)
+            {
+                listWithPages.Add(Convert.ToInt32( tmp.GetAttribute("text")));
+            }  
+
+            return listWithPages.Max();
+        }
 
         async Task<string> GetHtmlPage()
         {
